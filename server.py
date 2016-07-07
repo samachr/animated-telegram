78812e9f296952a724d1a20c50758c94037c6934
Samuel Christensen <samachr@gmail.com>
2016-07-06 21:18:07 -0600

    basic mongo integration

--- server.py
+++ server.py
@@ -5,16 +5,21 @@ app = Flask(__name__)
 ipaddr = os.getenv("IP", "0.0.0.0")
 port = int(os.getenv("PORT", 3000))
 
-# server globals
-tracking={}
+from pymongo import MongoClient
+MONGODB_URI = 'mongodb://pythontest:pythonpass@ds015915.mlab.com:15915/heroku_l6st7jg1'
+client = MongoClient(MONGODB_URI)
+db = client.get_default_database()
+testdb = db['test']
 
 @app.route('/api/input_example', methods = ['POST', 'GET'])
 def save_search():
+    global testdb
     input_param = request.args.get("search")
-    if input_param in tracking:
-        tracking[input_param] += 1
+    result = testdb.find_one({"search_text":input_param})
+    if result:
+        testdb.update({"search_text":input_param}, {"$set": {"frequency": result["frequency"] + 1}})
     else:
-        tracking[input_param] = 1
+        testdb.insert({"search_text":input_param, "frequency":1})
 
     return input_param
 
@@ -24,8 +29,9 @@ def get_json_stuff():
 
 @app.route('/', methods = ['GET'])
 def index():
-    global tracking
-    return render_template('index.html', tab="", tracking=tracking)
+    global testdb
+    docs = [doc for doc in testdb.find()]
+    return render_template('index.html', tab="", tracking=docs)
 
 @app.route('/app.js', methods = ['GET'])
 def appjs():
--- templates/app.js
+++ templates/app.js
@@ -1,8 +1,8 @@
 function search(song) {
   var searchsong = song || $('#search_text').val()
-  $("#guitartabcontent").html("loading...");
+  $("#response").html("loading...");
   $("#submit_button").html('<span class="glyphicon glyphicon-time"></span>');
-  $("#guitartabcontent").load("/api/input_example?search="+encodeURIComponent(searchsong), function(){
+  $("#response").load("/api/input_example?search="+encodeURIComponent(searchsong), function(){
     $("#submit_button").html('<span class="glyphicon glyphicon-search"></span>');
   });
 }
